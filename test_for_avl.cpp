317df7b4c704e6fc6364b8cbb1540f2e2136f6c6
Tayar <tayar.shai@gmail.com>
2022-04-18 17:20:15 +0300

    first

---
+++ test_for_avl.cpp
@@ -0,0 +1,16 @@
+#include "AvlTree.h"
+#include "iostream"
+#include "employee.h"
+using std::string;
+
+
+
+int main(){
+    AvlTree<Employee> tree;
+    Employee e1(1,100,1);
+    Employee e2(2,200,2);
+    Employee e3(5,500,5);
+    tree.insert(e3);
+    tree.insert(e1);
+    return 0;
+}
